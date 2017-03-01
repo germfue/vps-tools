98795882fd485b31b3f9f6697f4579db3d9d9727
Germán Fuentes Capella <github@gfc.33mail.com>
2017-03-01 19:10:01 +0100

    support for queries where there is an element missing

--- tests/vultr/query_tests.py
+++ tests/vultr/query_tests.py
@@ -2,24 +2,24 @@
 
 # Copyright (c) 2016, Germán Fuentes Capella <development@fuentescapella.com>
 # BSD 3-Clause License
-#
+# 
 # Copyright (c) 2017, Germán Fuentes Capella
 # All rights reserved.
-#
+# 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-#
+# 
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-#
+# 
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-#
+# 
 # * Neither the name of the copyright holder nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,29 +31,18 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from ast import literal_eval
-from vps.console import display
-from .key import get_key
+import unittest
+from invoke import Context
+from vps.vultr.query import query
 
 
-def query(ctx, q, criteria):
-    """
-    Query a Vultr endpoint
-    """
-    result = q(get_key())
-    if result:
-        result = list(result.values())
-        if criteria:
-            if isinstance(criteria, str):
-                criteria = literal_eval(criteria)
+class TestQuery(unittest.TestCase):
 
-            def _filter(a_dict):
-                for k, v in criteria.items():
-                    if (v and not a_dict.get(k)) or a_dict.get(k) != v:
-                        return False
-                return True
+    def test_missing_field(self):
+        ctx = Context()
+        ctx.config.run.echo = False
+        result = { 2: {'field1': 2}, 3: {'field1': 3, 'field2': 8}}
+        filtered_result = query(ctx, lambda x: result, {'field2': None})
+        self.assertEqual(1, len(filtered_result))
+        self.assertFalse(filtered_result[0].get('field2'))
 
-            result = list(filter(_filter, result))
-        if result and ctx.config.run.echo:
-            display(result)
-    return result
